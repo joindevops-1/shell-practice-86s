7db75de0eae122cab116efeecf94714d6c06b8c7
sivakumar reddy <info@joindevops.com>
2025-09-18 12:39:44 +0530

    session-14

---
+++ 04-varialbes.sh
@@ -0,0 +1,8 @@
+#!/bn/bash
+PERSON1=$1
+PERSON2=$2
+
+echo "$PERSON1:: Hey $PERSON2, nice meeting you in Alaska!"
+echo "$PERSON2:: Hello $PERSON1, yes… finally not on Zoom."
+echo "$PERSON1:: So… did we make a deal?"
+echo "$PERSON2:: If Ukraine joins the Russia, then consider it is deal"
