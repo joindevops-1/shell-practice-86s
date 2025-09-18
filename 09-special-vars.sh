9fc88cce22392cf2096fc67e363194650b7f8a09
sivakumar reddy <info@joindevops.com>
2025-09-18 20:11:00 +0530

    session-14

--- 09-special-vars.sh
+++ 09-special-vars.sh
@@ -7,5 +7,5 @@ echo "Current Directory: $PWD"
 echo "User running this script: $USER"
 echo "Home directory of user: $HOME"
 echo "PID of the script: $$"
-sleep 10 &
+sleep 100 &
 echo "PID of last command in background: $!"
